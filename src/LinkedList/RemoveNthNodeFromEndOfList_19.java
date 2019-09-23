cb6ef2296daa513b7be69581607e7bb95368ec7d
MuggleLee <770796059@qq.com>
2019-09-23 18:00:26 +0800

    Add

---
+++ src/LinkedList/RemoveNthNodeFromEndOfList_19.java
@@ -0,0 +1,103 @@
+package LinkedList;
+
+/**
+ * @author Muggle Lee
+ * @Date: 2019/9/23 10:18
+ * <p>
+ * 问题描述：
+ * Given a linked list, remove the n-th node from the end of list and return its head.
+ * <p>
+ * Example:
+ * <p>
+ * Given linked list: 1->2->3->4->5, and n = 2.
+ * <p>
+ * After removing the second node from the end, the linked list becomes 1->2->3->5.
+ * Note:
+ * <p>
+ * Given n will always be valid.
+ */
+public class RemoveNthNodeFromEndOfList_19 {
+
+    /**
+     * 看别人的算法过程
+     *
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode node = new ListNode(0);
+        ListNode result = node;
+        for (int i = 1; i <= n; i++) {
+            fast = fast.next;
+        }
+        while(fast!=null){
+            node.next = slow;
+            node = node.next;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        node.next = slow.next;
+        return result.next;
+    }
+
+//    public ListNode removeNthFromEnd(ListNode head, int n){
+//        ListNode node = reverse(head);
+//        ListNode tem = new ListNode(0);
+//        ListNode result = tem;
+//        int x = 0;
+//        while (node != null) {
+//            x++;
+//            if (x == n) {
+//                ListNode nextNode = node.next;
+//                tem.next = nextNode;
+//                return reverse(result.next);
+//            }
+//            tem.next = node;
+//            node = node.next;
+//            tem = tem.next;
+//        }
+//        return reverse(result.next);
+//    }
+//
+//    public ListNode reverse(ListNode head) {
+//        ListNode pre = null;
+//        while (head != null) {
+//            ListNode nextNode = head.next;
+//            head.next = pre;
+//            pre = head;
+//            head = nextNode;
+//        }
+//        return pre;
+//    }
+
+    public static void main(String[] args) {
+
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
+        ListNode listNode5 = new ListNode(5);
+        ListNode listNode6 = new ListNode(6);
+        ListNode listNode7 = new ListNode(7);
+        ListNode listNode8 = new ListNode(8);
+        ListNode listNode9 = new ListNode(9);
+        ListNode listNode10 = new ListNode(10);
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+        listNode5.next = listNode6;
+        listNode6.next = listNode7;
+        listNode7.next = listNode8;
+        listNode8.next = listNode9;
+        listNode9.next = listNode10;
+
+
+        RemoveNthNodeFromEndOfList_19 removeNthNodeFromEndOfList = new RemoveNthNodeFromEndOfList_19();
+        ListNode node = removeNthNodeFromEndOfList.removeNthFromEnd(listNode1, 2);
+        System.out.println(node);
+    }
+}
---
+++ src/LinkedList/RotateList_61.java
@@ -0,0 +1,90 @@
+package LinkedList;
+
+/**
+ * @author Muggle Lee
+ * @Date: 2019/9/23 13:35
+ * 问题描述：
+ * Given a linked list, rotate the list to the right by k places, where k is non-negative.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: 1->2->3->4->5->NULL, k = 2
+ * Output: 4->5->1->2->3->NULL
+ * Explanation:
+ * rotate 1 steps to the right: 5->1->2->3->4->NULL
+ * rotate 2 steps to the right: 4->5->1->2->3->NULL
+ * Example 2:
+ * <p>
+ * Input: 0->1->2->NULL, k = 4
+ * Output: 2->0->1->NULL
+ * Explanation:
+ * rotate 1 steps to the right: 2->0->1->NULL
+ * rotate 2 steps to the right: 1->2->0->NULL
+ * rotate 3 steps to the right: 0->1->2->NULL
+ * rotate 4 steps to the right: 2->0->1->NULL
+ */
+public class RotateList_61 {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
+        int size = 0;
+        ListNode tem = head;
+        ListNode node = new ListNode(0);
+        ListNode result = node;
+        while (tem != null) {
+            size++;
+            tem = tem.next;
+        }
+        // 如果旋转的次数等于链表的长度，则直接返回链表对象，不需要改动。
+        if (size == k) {
+            return head;
+        }
+        k %= size;
+        for (int i = 1; i <= size - k; i++) {
+            ListNode nextNode = head.next;
+            head.next = null;
+            node.next = head;
+            head = nextNode;
+            node = node.next;
+        }
+        tem = new ListNode(0);
+        ListNode tail = tem;
+        while (head != null) {
+            ListNode nextNode = head.next;
+            head.next = null;
+            tem.next = head;
+            tem = tem.next;
+            head = nextNode;
+        }
+        tem.next = result.next;
+        return tail.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
+//        ListNode listNode5 = new ListNode(5);
+//        ListNode listNode6 = new ListNode(6);
+//        ListNode listNode7 = new ListNode(7);
+//        ListNode listNode8 = new ListNode(8);
+//        ListNode listNode9 = new ListNode(9);
+//        ListNode listNode10 = new ListNode(10);
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+//        listNode4.next = listNode5;
+//        listNode5.next = listNode6;
+//        listNode6.next = listNode7;
+//        listNode7.next = listNode8;
+//        listNode8.next = listNode9;
+//        listNode9.next = listNode10;
+
+
+        RotateList_61 rotateList = new RotateList_61();
+        ListNode node = rotateList.rotateRight(listNode1, 2);
+        System.out.println(node);
+    }
+}
